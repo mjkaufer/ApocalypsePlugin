c111721957423fccc3d5ca8f4eb96b3f429f34f3
mjkaufer <mjkaufer@gmail.com>
2014-12-30 00:49:49 -0500

    Lots of stuff
    Chest support
    Random chest items
    Random dispersement
    Random quantities and damage
    Chest timer
    Messages when things happen
    Permissions to use admin commands

--- src/org/kaufer/matthew/ApocalypsePlugin/Apocalypse.java
+++ src/org/kaufer/matthew/ApocalypsePlugin/Apocalypse.java
@@ -5,13 +5,19 @@ import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.World;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
@@ -25,6 +31,10 @@ public class Apocalypse extends JavaPlugin {
 	
 	private int magnitude = 10;//how far away zombie will spawn
 	private Server s = this.getServer();
+	
+	private Material[] items = new Material[]{Material.CARROT, Material.MELON, Material.APPLE, Material.COOKED_FISH, Material.COOKED_CHICKEN, Material.TORCH, Material.STICK, Material.COBBLESTONE, Material.FLINT, Material.COOKED_BEEF, Material.FEATHER, Material.ARROW, Material.IRON_INGOT, Material.COOKIE, Material.BREAD, Material.ROTTEN_FLESH, Material.MUSHROOM_SOUP, Material.GOLDEN_APPLE};
+	private Material[] tools = new Material[]{Material.STONE_PICKAXE, Material.BOW, Material.STONE_SWORD, Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_BOOTS, Material.IRON_SWORD, Material.LEATHER_LEGGINGS, Material.IRON_BOOTS, Material.CHAINMAIL_HELMET, Material.IRON_LEGGINGS, Material.IRON_CHESTPLATE, Material.GOLD_CHESTPLATE, Material.DIAMOND, Material.DIAMOND_SWORD, Material.DIAMOND_HELMET};
+	
 	@Override
     public void onDisable()
     {
@@ -57,12 +67,20 @@ public class Apocalypse extends JavaPlugin {
             
             
             BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-            scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+            scheduler.scheduleSyncRepeatingTask(this, new Runnable() {//scheduler to add zombies
                 @Override
                 public void run() {
                     addZombies();
                 }
             }, 0L, 20L * 45L);//20 ticks is a second
+            
+            
+            scheduler.scheduleSyncRepeatingTask(this, new Runnable() {//scheduler to reset chests
+                @Override
+                public void run() {
+                    updateChests();
+                }
+            }, 0L, 20L * 30L * 60L);//every 30 minutes
             
     }
    
@@ -72,8 +90,17 @@ public class Apocalypse extends JavaPlugin {
             Player player = (Player)sender;
             if(cmd.getName().equalsIgnoreCase("apocalypse"))
             {
-            	if(args.length > 0 && args[0].equalsIgnoreCase("zombie")){
-            		addZombies();
+            	if(args.length > 0){
+            		if(!player.hasPermission("Apocalypse.*")){//not allowed to do fancy commands
+            			player.sendMessage("Need Apocalypse.* permission!");
+            			return true;
+            		}
+	            	if(args[0].equalsIgnoreCase("zombie")){
+	            		addZombies();
+	            	}
+	            	else if(args[0].equalsIgnoreCase("chest")){
+	            		updateChests();
+	            	}
             	}
         		else{
             		PluginDescriptionFile p = this.getDescription();
@@ -90,11 +117,11 @@ public class Apocalypse extends JavaPlugin {
     	List<Player> players = apocalypseWorld.getPlayers();
     	
     	for(Player p : players){
-    		p.sendMessage(ChatColor.GREEN + "MMMMmmmmmmmm.... Braaaaainsssss...");
+//    		p.sendMessage(ChatColor.GREEN + "MMMMmmmmmmmm.... Braaaaainsssss...");
     		//TODO - make it so zombies don't spawn in nothing
     		//check if location to spawn at has air, if not, move up, modulus around possibilities, when done, quit
     		Location l = p.getLocation();
-    		for(int i = 0; i < (int)(Math.random() * 4 + 2); i++){//spawn a few zombies
+    		for(int i = 0; i < (int)(Math.random() * 4 + 4); i++){//spawn a few zombies
         		float yaw = l.getYaw();//Yaw of person
         		//we want the zombie to spawn behind the person, more or less, so we'll give it an offset of +-30
         		float angle = 60;//variance
@@ -103,7 +130,7 @@ public class Apocalypse extends JavaPlugin {
         		//with yaw, 90 maps to -x, -90 maps to x, so we're going to negate x values
         		yaw = yaw * (float)(Math.PI) / 180.0f;//to radians, very important
 
-        		Vector sub = new Vector(-1 * magnitude * Math.sin(yaw),0,magnitude * Math.cos(yaw));//sin is to x because of weirdass layout
+        		Vector sub = new Vector(-1 * magnitude * Math.sin(yaw),-2,magnitude * Math.cos(yaw));//sin is to x because of weirdass layout - the -2 y val is so the monster spawns in the air 
         		Location mobPosition = l.subtract(sub);
         		apocalypseWorld.spawnEntity(mobPosition, EntityType.ZOMBIE);
     		}
@@ -111,6 +138,58 @@ public class Apocalypse extends JavaPlugin {
     	}
     	
     	return true;
+    }
+    
+    public boolean updateChests(){//refills chests with random shite
+    	World apocalypseWorld = this.getServer().getWorld("world");
+    	
+    	for(Chunk c : apocalypseWorld.getLoadedChunks()){
+    		for(BlockState b : c.getTileEntities()){
+	    		if(b instanceof Chest){
+		    		Chest chest = (Chest) b;
+		    		Inventory ci = chest.getBlockInventory();
+		    		
+		    		ItemStack[] ris = randomItemStack();
+	    			ci.clear();//empty that chest!
+
+	    			int size = ci.getSize();
+	    			
+	    			for(int i = 0; i < ris.length; i++){
+	    				int index = (int)(Math.random() * size);
+	    				ci.setItem(index, ris[i]);//we might overlap, but that's ok
+	    				
+	    			}
+	    		}
+    		}
+    	}
+    	
+    	s.broadcastMessage(ChatColor.AQUA + "[Apocalypse] " + ChatColor.GREEN + "Chests reset!");
+    	
+    	return true;
+    }
+    
+    public ItemStack[] randomItemStack(){
+    	
+    	int length = Math.min(items.length, (int)(Math.random() * Math.random() * 4 + 5));
+    	ItemStack[] ret = new ItemStack[length];
+    	for(int i = 0; i < ret.length; i++){
+    		if(i%4 == 0){//we'll do tools instead
+    			int index = (int)(Math.random() * Math.random() * tools.length);
+    			
+    			
+        		Material m = tools[index];
+        		ret[i] = new ItemStack(m, 1, (short)Math.max(Math.random() * Math.random() * 100, 10));//add it
+
+    			continue;
+    		}
+    		int index = (int)(Math.random() * Math.random() * items.length);//we square math.random so it stays near the front of the array, so more rare stuff goes in the back of the items array
+    		int quantity = (int)Math.ceil((items.length - index) * (0.5+Math.random()));//more of items in front vs those in back - ceil so we don't get 0 of an item
+//    		ret[i] = items[index];
+    		Material m = items[index];
+    		ret[i] = new ItemStack(m, quantity);//add it
+    		
+    	}
+    	return ret;
     }
 
 
